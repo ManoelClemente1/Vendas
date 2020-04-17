f8bacb843f8c12ea7801327357c2bc2498fc6624
Manoel C <manoelclemente9@outlook.com>
2020-04-17 07:58:37 -0300

    Query methods

--- src/main/java/com/mc/zero/repositorio/Clientes.java
+++ src/main/java/com/mc/zero/repositorio/Clientes.java
@@ -1,9 +1,17 @@
 package com.mc.zero.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mc.zero.entity.Cliente;
 
 public interface Clientes extends JpaRepository<Cliente, Integer>{
-
+	
+	List<Cliente> findByNomeLike(String nome);
+	
+	List<Cliente> findByNomeOrId(String nome, Integer id);
+	
+	boolean existsCliente(String nome);
+	
 }
