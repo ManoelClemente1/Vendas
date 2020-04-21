d7b357e97c8f891bdb2d61970483bac96ae46026
Manoel C <manoelclemente9@outlook.com>
2020-04-20 22:18:20 -0300

    @Query methods

--- src/main/java/com/mc/zero/repositorio/Clientes.java
+++ src/main/java/com/mc/zero/repositorio/Clientes.java
@@ -3,10 +3,15 @@ package com.mc.zero.repositorio;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.mc.zero.entity.Cliente;
 
 public interface Clientes extends JpaRepository<Cliente, Integer>{
+	
+	@Query(value="select c from Cliente c were c.nome like :nome")
+	List<Cliente> findByName(@Param("nome") String nome);
 	
 	List<Cliente> findByNomeLike(String nome);
 	
