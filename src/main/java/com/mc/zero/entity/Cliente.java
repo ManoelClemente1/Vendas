f81ffce518100a178f7f2726e1348603d6fbd935
Manoel C <manoelclemente9@outlook.com>
2020-04-22 08:43:52 -0300

    Mapeando entidade pedido

--- src/main/java/com/mc/zero/entity/Cliente.java
+++ src/main/java/com/mc/zero/entity/Cliente.java
@@ -1,10 +1,13 @@
 package com.mc.zero.entity;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +21,10 @@ public class Cliente {
 
     @Column(name = "nome", length = 100)
     private String nome;
+    
+    @OneToMany(mappedBy = "cliente")
+    
+    private Set<Pedido> pedido;
 
     public Cliente() {
     }
